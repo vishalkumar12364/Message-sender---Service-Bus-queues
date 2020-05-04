16434550bbd2fe2c38a33efa70e030584092264d
Vishal Kumar <kumarvishal12364@gmail.com>
2020-05-04 15:31:35 +0530

    Add project files.

---
+++ Message sender - Service Bus queues/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Message_sender___Service_Bus_queues
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://senderservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GLycPbJtx8K1tl4oEzs5UVH9qvrhoJwVSRanjVRiZrU=";
+        const string QueueName = "MyQueue";
+        static IQueueClient queueClient;
+
+        public static async Task Main(string[] args)
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+           
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 1; i < numberOfMessagesToSend; i++)
+                {
+                   
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
